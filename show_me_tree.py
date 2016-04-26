d336cb42e60419c12dc06228ec2f5bd6e5ecca80
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-04-26 15:39:07 +0200

    xrange

--- show_me_tree.py
+++ show_me_tree.py
@@ -1,40 +1,40 @@
 import argparse
 import sys
 
-try:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--high", type=int,
-                        help="Display a hight of a tree",
-                        required=True)
-    parser.add_argument("--char",type=str,
-                        help="Display a symbol of a tree",
-                        default="'Brak symbolu!'",
-                        required=True,
-                        choices=("*", "@", "#", "o", "O", "0", "8"))
 
-    args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--high", type=int,
+                    help="Display a hight of a tree",
+                    required=True)
+parser.add_argument("--char",type=str,
+                    help="Display a symbol of a tree",
+                    default="'Brak symbolu!'",
+                    required=True,
+                    choices=("*", "@", "#", "o", "O", "0", "8"))
 
-
-    if 3 <= args.high <= 24:
-
-        print args.char.center(120)
-
-        for i in range(args.high):
-            j = (i * 3)
-            print (j * args.char).center(120)
-
-        print ("|").center(120)
-        print ("^").center(120)
+args = parser.parse_args()
 
 
-    elif 0 <= args.high <= 2:
-        sys.exit(1)
+if 3 <= args.high <= 24:
 
-    elif args.high > 24:
-        sys.exit(1)
+    print args.char.center(120)
 
-    else:
-        print "Blad"
+    for i in xrange(1, args.high):
+        j = (i * 3)
+        print (j * args.char).center(120)
 
-except: print "Wprowadz poprawna wartosc!"
+    print ("|").center(120)
+    print ("^").center(120)
+
+
+elif 0 <= args.high <= 2:
+    sys.exit(1)
+
+elif args.high > 24:
+    sys.exit(1)
+
+else:
+    print "Blad"
+
+
 
