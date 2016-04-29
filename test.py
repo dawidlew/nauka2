b5af72ff83802d81dd793052039d299be289c49d
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-04-29 12:27:31 +0200

    cd

--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-str = "this is string example....wow!!!";
+import argparse
+import sys
 
-print str.center(120)
+parser = argparse.ArgumentParser()
 
+parser.add_argument("--high", type=int,
+                    help="Display a hight of a tree",
+                    required=True)
+parser.add_argument("--savetofile",type=str,
+                    help="Path to the file",
+                    required=False)
 
+args = parser.parse_args()
+
+prompt = '> Please input directory for output file:'
+
+if not args.savetofile:
+    print prompt
+
+else:
+    file = open(args.savetofile + str(args.high) + ".txt", "w")
+
+    file.write("hello world in the new file")
+
+    file.close()
