31f1677bd7d1957c7a2ada5bb4a5e9c51a0c47bd
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-05-11 11:48:46 +0200

    cd 5

--- show_me_tree.py
+++ show_me_tree.py
@@ -18,40 +18,39 @@ parser.add_argument("--savetofile",type=str,
 
 args = parser.parse_args()
 
-def writer(high, aaa):
-    os.chdir(directory)
+def writer(aaa, bbb):
+    os.chdir(bbb)
     file = open(str(args.high) + ".txt", "w")
-    file.write(directory)
+    file.write(bbb)
     file.close()
+    print choinka()
 
-def exit(katalog):
-    print "[ERROR] Problem with creating directory " + katalog
+def exit(wys):
+    print '[ERROR] Unexpected height: ' + wys
     sys.exit(3)
 
-def choinka(high):
-    n = 3 + (high * 2)
+def choinka():
+    n = 3 + (args.high * 2)
     print args.char.center(n)
-    for i in xrange(1, high):
+    for i in xrange(1, args.high):
         j = i * 2
         print ((j + 1) * args.char).center(n)
     print ('|').center(n)
     print ('^').center(n)
 
-# try:
-if 3 <= args.high <= 24:
-    if not args.savetofile:
-        directory = raw_input('Please input directory for output file > ')
-        writer(args.high, directory)
-    writer(args.high, args.savetofile)
+def wysokosc(katalog):
+    if 3 <= args.high <= 24:
+        writer(args.high, katalog)
+    elif 0 <= args.high <= 2:
+        exit(args.high)
+        sys.exit(1)
+    elif args.high > 24:
+        exit(args.high)
+        sys.exit(1)
 
-elif 0 <= args.high <= 2:
-    exit(directory)
-    sys.exit(1)
-
-elif args.high > 24:
-    exit(directory)
-    sys.exit(1)
-# except:
-#     # exit(directory)
-#     print "1"
+if not args.savetofile:
+    directory = raw_input('Please input directory for output file > ')
+    wysokosc(directory)
+else:
+    wysokosc(args.savetofile)
 
