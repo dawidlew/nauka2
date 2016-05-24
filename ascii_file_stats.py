1e9fbf8e0c3eeae528e60debf3dae92c75e4a5ed
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-05-24 10:14:59 +0200

    v.5

--- ascii_file_stats.py
+++ ascii_file_stats.py
@@ -12,25 +12,31 @@ def read_file_content(path):
     return content
 
 def stat(content):
-    results = []
+    results = {}
     col = collections.Counter(content)
     for letter in string.printable:
         if col[letter] > 0:
             if letter not in string.whitespace:
-                print '%s : %d, %d' % (letter, col[letter], ord(letter))
-
-                results.extend([letter, col[letter], ord(letter), os.linesep])
-
-                results.sort()
-            print results
-
+                # print '%s : %d, %d' % (letter, col[letter], ord(letter))
+                results[ord(letter)] = col[letter]
+    return results
 
 
 
 def process(path):
     content = read_file_content(path)
     #content = read_from_url(path)
-    stat(content)
+    v = stat(content)
+    b = sorted(v.items())
+    v = collections.OrderedDict(b)
+    print str(v)
+    print str(v.items())
+    for key, val in v.items():
+        print str(chr(key)) + ': ' + str(val)
+
+
+    # for el in v:
+    #     print str(chr(el)) + ': ' + str(d[el])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
