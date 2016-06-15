49465d6cd1355a83afb00f8ea9764b6b18d18bbf
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-06-15 15:34:19 +0200

    Plik testowy

--- testowy.py
+++ testowy.py
@@ -1,42 +1,41 @@
-def print_sorted_list(data, rows=0, columns=0):
-    """
-    Prints sorted item of the list data structure formated using
-    the rows and columns parameters
-    """
-    print the_list
-    if not data:
-        return
 
-    if rows:
-        # column-wise sorting
-        # we must know the number of rows to print on each column
-        # before we print the next column. But since we cannot
-        # move the cursor backwards (unless using ncurses library)
-        # we have to know what each row with look like upfront
-        # so we are basically printing the rows line by line instead
-        # of printing column by column
-        lines = {}
-        for count, item in enumerate(sorted(data)):
-            lines.setdefault(count % rows, []).append(item)
-        for key, value in sorted(lines.items()):
-            for item in value:
-                print '{:<10}'.format(item),
-            print
-    elif columns:
-        # row-wise sorting
-        # we just need to know how many columns should a row have
-        # before we print the next row on the next line.
-        for count, item in enumerate(sorted(data), 1):
-            # print item,
-            print '{:<10}'.format(str(chr(key)) + ': ' + str(value)),
-            if count % columns == 0:
-                print
+
+
+
+
+
+def show_item(word_or_char, count, is_word):
+    if word:
+        formant = '{:<15}: {:}'
     else:
-        print sorted(data)  # the default print behaviour
+        formant = '{}: {:<15}'
+    print formant.format(chr(word_or_char), count)
 
 
-if __name__ == '__main__':
-    # the_list = ['apricot','banana','apple','car','coconut','baloon','bubble']
-    the_list = [(46, 1), (48, 6), (49, 14), (50, 2), (51, 6), (52, 5), (53, 5), (54, 2), (55, 3), (56, 5), (57, 5), (65, 6), (68, 6), (69, 1), (70, 1), (91, 1), (93, 1), (97, 4), (98, 2), (99, 1), (100, 9), (101, 9), (102, 5), (103, 4), (104, 7), (105, 2), (106, 1), (107, 3), (109, 1), (110, 3), (111, 2), (112, 5), (113, 3), (114, 5), (115, 2), (116, 5), (117, 3), (118, 1), (119, 5), (120, 1), (121, 4)]
 
-    print_sorted_list(the_list, columns=3, rows=13)
+
+
+
+
+
+    def print_sorted_list(data, rows=0, columns=0, print_solution=False):
+        if rows:
+            lines = {}
+            for count, item in enumerate(data):
+                lines.setdefault(count % rows, []).append(item)
+            for key, value in lines.items():
+                for item in value:
+                    if print_solution:
+                        print '{:<15}: {:}'.format((item[0]), item[1]),
+                    else:
+                        print '{}: {:<15}'.format(chr(item[0]), item[1]),
+                print
+
+    elif columns:
+    for count, item in enumerate(data, 1):
+        print item,
+        if count % columns == 0:
+            print
+
+else:
+print data
