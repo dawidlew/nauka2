c26eb9508f6810f2528b3ff2a7833c78d3a1f0d8
U-ALLEGROGROUP\dawid.lewandowicz <dawid.lewandowicz@allegrogroup.com>
2016-06-30 10:49:21 +0200

    v.4

--- apples_webcrawler.py
+++ apples_webcrawler.py
@@ -1,24 +1,15 @@
 # coding=utf-8
 import requests, bs4
+import pprint
+
 res = requests.get('http://www.sadbiznes.pl/notowania-jablek/7')
 text = bs4.BeautifulSoup(res.text, "html.parser")
-
 
 def get_column_data(data_selector, class_value_selector):
     results=[]
     for i in text.find_all(data_selector, class_=class_value_selector):
         results.append(i.string.strip())
     return results
-
-
-col_dict = {
-    "for_day": [],
-    "name": [],
-    "note_day": [],
-    "city": [],
-    "price_min": [],
-    "price_max": []
-}
 
 selector_info = {
     "for_day":["td", "views-field views-field-changed"],
@@ -29,14 +20,9 @@ selector_info = {
     "price_max": ["td", "views-field views-field-field-cena-max-value"]
 }
 
-for i in col_dict:
-    s = 0
-    for selector in selector_info:
-        col_dict[i] = get_column_data(selector_info.items()[s][1][0], selector_info.items()[s][1][1])
-        s = s + 1
+col_dict = {}
 
-print col_dict
+for key_name, (selector_first, selector_second) in selector_info.iteritems():
+    col_dict[key_name] = get_column_data(selector_first, selector_second)
 
-
-print selector_info.items()[0][1][0]
-print selector_info.items()[0][1][1]
+pprint.pprint(col_dict)
