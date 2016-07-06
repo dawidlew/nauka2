93bfa9f2319bddc8b22ed7cca0dd21e3ccb7ad54
DawidLew <dawid.stamdo@gmai.com>
2016-07-06 21:08:57 +0200

    Timestamp from sql

--- apples_agg.py
+++ apples_agg.py
@@ -8,22 +8,23 @@ timestamp = int(time.time())
 print timestamp
 
 # cursor.execute("drop table note_agg")
-# cursor.execute("create table note_agg (name, city, avg_price_min, avg_price_max)")
+# cursor.execute("create table note_agg (name, city, avg_price_min, avg_price_max, timestamp)")
 
 avg_price = cursor.execute("select DISTINCT name, city, round(avg(replace(price_min, ',', '.')),2), "
-                           "round(avg(replace(price_max, ',', '.')),2) from note "
+                           "round(avg(replace(price_max, ',', '.')),2), strftime('%s','now') from note "
                            "where for_day in (select DISTINCT for_day from note order by for_day desc limit 10) "
                            "group by name, city")
 avg_price_data = avg_price.fetchall()
 
+print avg_price_data
 # print avg_price_data[1]
 # print avg_price_data[1][0]
 
-stmt = "insert into note_agg(name, city, avg_price_min, avg_price_max) VALUES(?, ?, ?, ?)"
+stmt = "insert into note_agg(name, city, avg_price_min, avg_price_max, timestamp) VALUES(?, ?, ?, ?, ?)"
 cursor.executemany(stmt, avg_price_data)
 conn.commit()
 
-results = cursor.execute("select name, city, avg_price_min, avg_price_max from note_agg")
-print(str(results.fetchall()))
+results = cursor.execute("select name, city, avg_price_min, avg_price_max, timestamp from note_agg")
+# print(str(results.fetchall()))
 
 cursor.close()
